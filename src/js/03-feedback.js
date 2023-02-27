2d774582162c5e652453ed98931af89a7ce0b32f
Demag0g1 <konievanatol@gmail.com>
2023-02-27 23:09:50 +0200

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,53 @@
+import { throttle } from 'lodash';
 
+const form = document.querySelector('.feedback-form');
+const email = document.querySelector('input[name="email"]');
+const message = document.querySelector('textarea[name="message"]');
+const LOCALSTORAGE_KEY = 'feedback-form-state';
+
+form.addEventListener(
+  'input',
+  throttle(evt => {
+    const objectSave = { email: email.value, message: message.value };
+    localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(objectSave));
+  }, 500)
+);
+
+form.addEventListener('submit', evt => {
+  evt.preventDefault();
+  if (email.value === '' || message.value === '') {
+    return alert(' Fill in all fields of the form!');
+  }
+  console.log({ email: email.value, message: message.value });
+  form.reset();
+  localStorage.removeItem(LOCALSTORAGE_KEY);
+});
+
+const save = (key, value) => {
+  try {
+    const serializedState = JSON.stringify(value);
+    localStorage.setItem(key, serializedState);
+  } catch (error) {
+    console.error('Set state error: ', error.message);
+  }
+};
+
+const load = key => {
+  try {
+    const serializedState = localStorage.getItem(key);
+    return serializedState === null ? undefined : JSON.parse(serializedState);
+  } catch (error) {
+    console.error('Get state error: ', error.message);
+  }
+};
+
+export default {
+  save,
+  load,
+};
+const storageData = load(LOCALSTORAGE_KEY);
+
+if (storageData) {
+  email.value = storageData.email;
+  message.value = storageData.message;
+}
