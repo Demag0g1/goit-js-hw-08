6c6844cf888bf68f2cb582cae605b5e88ebe9e4c
Demag0g1 <konievanatol@gmail.com>
2023-02-27 18:48:24 +0200

    first exp its work

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,19 +1,14 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
-// Change code below this line
 import SimpleLightbox from 'simplelightbox';
-
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
 const imgConteiner = document.querySelector('.gallery');
 const createGalleryItem = galleryItems
   .map(item => {
-    return `<div class="gallery__item">
-        <a class="gallery__link" href="${item.original}" >
-        <img class="gallery__image" data-attribute="${item.original}" src="${item.preview}" alt="${item.description}" title="${item.description}" width="340"/>
-         
-        </a>  
-    </div>`;
+    return `<a class="gallery__item" href="${item.original}" >
+        <img class="gallery__image" data-attribute="${item.preview}" src="${item.preview}" alt="${item.description}" title="${item.description}" />
+        </a>`;
   })
   .join('');
 
@@ -27,28 +22,20 @@ document.addEventListener('click', e => {
   }
 
   const imgSelected = e.target.getAttribute('data-attribute');
-
-  const template = basicLightbox.create(
-    `
-    <img src="${imgSelected}">
-    `,
-
-    {
-      onShow: () => {
-        document.addEventListener('keydown', closeModal);
-      },
-
-      onClose: () => {
-        document.removeEventListener('keydown', closeModal);
-      },
-    }
-  );
-
-  template.show();
-
-  function closeModal(e) {
-    if (e.key === 'Escape') {
-      template.close();
-    }
-  }
 });
+let lightbox = new SimpleLightbox('.gallery a', {
+  onShow: () => {
+    document.addEventListener('keydown', closeModal);
+  },
+
+  onClose: () => {
+    document.removeEventListener('keydown', closeModal);
+  },
+  captionDelay: 250,
+});
+
+function closeModal(e) {
+  if (e.key === 'Escape') {
+    template.close();
+  }
+}
